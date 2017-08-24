1d4518fef03167ed8c2c4c8bb70b496a0ba3c6b1
Rutul Patel <rutul-patel@live.com>
2017-08-23 21:46:08 -0500

    Add book-list container with static data from reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+            { title: 'JS: the good parts'},
+            { title: 'Harry Potter' },
+            { title: 'The Dark Knight' },
+            { title: 'Screw that!' },
+            { title: 'Deep dive into Ruby'}
+        ]
+};
